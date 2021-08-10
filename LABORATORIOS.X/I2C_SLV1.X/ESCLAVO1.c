d5d93b15e70daa75ac25ca84be222f404d40a990
pat19218 <68085020+pat19218@users.noreply.github.com>
2021-08-09 18:43:49 -0600

    ok

--- LABORATORIOS.X/I2C_SLV1.X/ESCLAVO1.c
+++ LABORATORIOS.X/I2C_SLV1.X/ESCLAVO1.c
@@ -1,20 +1,14 @@
-/* File:   PROYECTO.c
- * Dispositivo:	PIC16f887
- * Author: isaac
- *
- * Compilador:	pic-as (v2.30), MPLABX V5.45
- * 
- * Programa: Valores de ADC mediante LCD e I2C. ESCLAVO
- * Hardware: Pic 16f887, potenciometro, comunicación I2C
- * 
- * Created on 8 de agosto de 2021, 8:20
- * Last modification 8 de agosto de 2021, 
+/*
+ * File:   main.c
+ * Author: Pablo
+ * Ejemplo de uso de I2C Esclavo
+ * Created on 17 de febrero de 2020, 10:32 AM
  */
-
-// PIC16F887 Configuration Bit Settings
-// 'C' source line config statements
+//*****************************************************************************
+// Palabra de configuración
+//*****************************************************************************
 // CONFIG1
-#pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (RCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, RC on RA7/OSC1/CLKIN)
 #pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
 #pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
 #pragma config MCLRE = OFF      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
@@ -23,7 +17,7 @@
 #pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
 #pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
 #pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
-#pragma config LVP = OFF         // Low Voltage Programming Enable bit (RB3/PGM pin has PGM function, low voltage programming enabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
 
 // CONFIG2
 #pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
@@ -32,26 +26,29 @@
 // #pragma config statements should precede project file includes.
 // Use project enums instead of #define for ON and OFF.
 
-//-------------------------Librerias--------------------------------------------
-#include <xc.h>
+//*****************************************************************************
+// Definición e importación de librerías
+//*****************************************************************************
 #include <stdint.h>
 #include <pic16f887.h>
 #include "I2C.h"
-
-//--------------------------directivas del compilador---------------------------
-#define _XTAL_FREQ 8000000 //__delay_ms(x)
-
-//---------------------------variables------------------------------------------
-char datoadc;
+#include <xc.h>
+//*****************************************************************************
+// Definición de variables
+//*****************************************************************************
+#define _XTAL_FREQ 8000000
 uint8_t z;
-
-//--------------------------funciones-------------------------------------------
-
-//---------------------------interrupciones-------------------------------------
-
-void __interrupt()isr(void){
-    
-    if(PIR1bits.SSPIF == 1){ 
+char datoadc;
+//*****************************************************************************
+// Definición de funciones para que se puedan colocar después del main de lo 
+// contrario hay que colocarlos todas las funciones antes del main
+//*****************************************************************************
+void setup(void);
+//*****************************************************************************
+// Código de Interrupción 
+//*****************************************************************************
+void __interrupt() isr(void){
+   if(PIR1bits.SSPIF == 1){ 
 
         SSPCONbits.CKP = 0;
        
@@ -62,7 +59,7 @@ void __interrupt()isr(void){
             SSPCONbits.CKP = 1;         // Enables SCL (Clock)
         }
 
-        if(!SSPSTATbits.D_nA && !SSPSTATbits.R_nW) {//leer dato
+        if(!SSPSTATbits.D_nA && !SSPSTATbits.R_nW) {
             //__delay_us(7);
             z = SSPBUF;                 // Lectura del SSBUF para limpiar el buffer y la bandera BF
             //__delay_us(2);
@@ -72,7 +69,7 @@ void __interrupt()isr(void){
             PORTD = SSPBUF;             // Guardar en el PORTD el valor del buffer de recepción
             __delay_us(250);
             
-        }else if(!SSPSTATbits.D_nA && SSPSTATbits.R_nW){//mandar dato
+        }else if(!SSPSTATbits.D_nA && SSPSTATbits.R_nW){
             z = SSPBUF;
             BF = 0;
             SSPBUF = PORTB;
@@ -82,24 +79,43 @@ void __interrupt()isr(void){
         }
        
         PIR1bits.SSPIF = 0;    
-    }//fin interrupcion protocolo I2C
-    
+    }
 }
-
-//----------------------configuracion microprocesador---------------------------
-
+//*****************************************************************************
+// Main
+//*****************************************************************************
 void main(void) {
-   
+    setup();
+    //*************************************************************************
+    // Loop infinito
+    //*************************************************************************
+    while(1){
+        if(ADCON0bits.GO == 0){
+            datoadc = ADRESH;
+            __delay_us(50);     //con 6 micros segundos será suficiente se dejo
+                                //en 50 por fallos de software en proteus
+            ADCON0bits.GO = 1;
+        }
+        __delay_us(20);
+       PORTB = datoadc;
+    }
+    return;
+}
+//*****************************************************************************
+// Función de Inicialización
+//*****************************************************************************
+void setup(void){
     ANSEL = 0b00100000; //pin 1 analogico
-    ANSELH = 0x00;      // solo pines digitales
+    ANSELH = 0;
     
-    TRISA = 0x00;       //solo pines de salida
-    TRISB = 0x00;
-    TRISD = 0x00;
+    TRISB = 0;
+    TRISD = 0;
     TRISE = 0b001;      //primer pin de entrada
     
+    PORTB = 0;
+    PORTD = 0;
     OSCCONbits.IRCF = 0b111; //Config. de oscilacion 8MHz
-    OSCCONbits.SCS = 1;      //reloj interno
+    OSCCONbits.SCS = 1;      //reloj interno    
     
                             //Config. ADC
     ADCON1bits.ADFM = 0;    //Justificado a la izquierda
@@ -112,26 +128,5 @@ void main(void) {
     ADCON0bits.ADON = 1;    //activo el modulo
     ADCON0bits.GO = 1;  
     
-    
-                           //Estado inicial
-    PORTA = 0x00;
-    PORTC = 0x00;    
-    PORTB = 0x00;    
-    PORTD = 0x00;    
     I2C_Slave_Init(0x50);   
-    
-    
-    //------------------------------loop principal----------------------------------
-    while (1){
-        if(ADCON0bits.GO == 0){
-            datoadc = ADRESH;
-            __delay_us(50);     //con 6 micros segundos será suficiente se dejo
-                                //en 50 por fallos de software en proteus
-            ADCON0bits.GO = 1;
-        }
-        __delay_us(20);
-       PORTB = datoadc;
-    }
-    return;
 }
-
