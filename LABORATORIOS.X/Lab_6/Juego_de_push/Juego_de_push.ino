b136045e80aaea0e5cf7926acbbc044fec4d2bd5
pat19218 <68085020+pat19218@users.noreply.github.com>
2021-09-19 16:48:32 -0600

    Serial

--- LABORATORIOS.X/Lab_6/Juego_de_push/Juego_de_push.ino
+++ LABORATORIOS.X/Lab_6/Juego_de_push/Juego_de_push.ino
@@ -5,7 +5,7 @@
                 realiza por un contador de decadas
    Materiales:  TivaC LaunchPad TM4C123GXL, 16 leds, 16 res
    Fecha de creacion: 13 de  septiembre 2021, 17:45
-   Ultima modificacion: 13 de septiembre 2021,
+   Ultima modificacion: 13 de septiembre 2021,20:09
 */
 
 #define rojo RED_LED    //pin 30 PF_1
@@ -32,6 +32,29 @@ void setup() {
   pinMode(verde, OUTPUT);     //los configuro como pines de salida
   pinMode(rojo, OUTPUT);
   pinMode(azul, OUTPUT);
+
+  pinMode(23, OUTPUT);  //j11
+  pinMode(24, OUTPUT);  //j12
+  pinMode(25, OUTPUT);  //j13
+  pinMode(26, OUTPUT);  //j14
+  pinMode(27, OUTPUT);  //j15
+  pinMode(28, OUTPUT);  //j16
+  pinMode(29, OUTPUT);  //j17
+  pinMode(10, OUTPUT);  //j18
+
+  pinMode(2, OUTPUT);  //j21
+  pinMode(3, OUTPUT);  //j22
+  pinMode(4, OUTPUT);  //j23
+  pinMode(5, OUTPUT);  //j24
+  pinMode(6, OUTPUT);  //j25
+  pinMode(7, OUTPUT);  //j26
+  pinMode(8, OUTPUT);  //j27
+  pinMode(9, OUTPUT);  //j28
+
+  Serial.begin(9600);
+  delay(1);
+  Serial.println("Presiona cualquier boton para iniciar el juego");
+  Serial.println("ganara el primero en presionar 8 veces \n");
 }
 
 void loop() {
@@ -42,27 +65,39 @@ void loop() {
 
   if ((estado1 == 0 || estado2 == 0) && juega == 0) { //cualquiera presionado
     juega = 1;
+    Serial.println("Empieza en: 3");
     digitalWrite(rojo, 1);
     digitalWrite(verde, 0);
     digitalWrite(azul, 0);
     delay(1500);
+    Serial.println("Empieza en: 2");
     digitalWrite(rojo, 1);
     digitalWrite(verde, 1);
     digitalWrite(azul, 0);
     delay(1500);
+    Serial.println("Empieza en: 1");
     digitalWrite(rojo, 0);
     digitalWrite(verde, 1);
     digitalWrite(azul, 0);
     delay(1500);
+    Serial.println("  Buena suerte  ");
     digitalWrite(rojo, 0);
     digitalWrite(verde, 0);
     digitalWrite(azul, 0);
+    estado1 = 0;
+    estado2 = 0;
   }
 
   if (juega) {
     if (estado1 == 0 && estado2 == 1) { //sw1 presionado, suma
       if (estado1 != old_estado1) {
         punteo1++;
+
+        Serial.print("Juagador 1 = ");
+        Serial.print(punteo1);
+        Serial.print("  Juagador 2 = ");
+        Serial.println(punteo2);
+
         digitalWrite(rojo, 1);
         digitalWrite(verde, 0);
       }
@@ -70,12 +105,183 @@ void loop() {
     else if (estado1 == 1 && estado2 == 0) { //sw2 presionado, suma
       if (estado2 != old_estado2) {
         punteo2++;
+
+        Serial.print("Juagador 1 = ");
+        Serial.print(punteo1);
+        Serial.print("  Juagador 2 = ");
+        Serial.println(punteo2);
+
         digitalWrite(rojo, 0);
         digitalWrite(verde, 1);
       }
     }
+    switch (punteo1) {
+      case 1:
+        digitalWrite(23, 1);
+        digitalWrite(24, 0);
+        digitalWrite(25, 0);
+        digitalWrite(26, 0);
+        digitalWrite(27, 0);
+        digitalWrite(28, 0);
+        digitalWrite(29, 0);
+        digitalWrite(10, 0);
+        break;
+      case 2:
+        digitalWrite(23, 0);
+        digitalWrite(24, 1);
+        digitalWrite(25, 0);
+        digitalWrite(26, 0);
+        digitalWrite(27, 0);
+        digitalWrite(28, 0);
+        digitalWrite(29, 0);
+        digitalWrite(10, 0);
+        break;
+      case 3:
+        digitalWrite(23, 0);
+        digitalWrite(24, 0);
+        digitalWrite(25, 1);
+        digitalWrite(26, 0);
+        digitalWrite(27, 0);
+        digitalWrite(28, 0);
+        digitalWrite(29, 0);
+        digitalWrite(10, 0);
+        break;
+      case 4:
+        digitalWrite(23, 0);
+        digitalWrite(24, 0);
+        digitalWrite(25, 0);
+        digitalWrite(26, 1);
+        digitalWrite(27, 0);
+        digitalWrite(28, 0);
+        digitalWrite(29, 0);
+        digitalWrite(10, 0);
+        break;
+      case 5:
+        digitalWrite(23, 0);
+        digitalWrite(24, 0);
+        digitalWrite(25, 0);
+        digitalWrite(26, 0);
+        digitalWrite(27, 1);
+        digitalWrite(28, 0);
+        digitalWrite(29, 0);
+        digitalWrite(10, 0);
+        break;
+      case 6:
+        digitalWrite(23, 0);
+        digitalWrite(24, 0);
+        digitalWrite(25, 0);
+        digitalWrite(26, 0);
+        digitalWrite(27, 0);
+        digitalWrite(28, 1);
+        digitalWrite(29, 0);
+        digitalWrite(10, 0);
+        break;
+      case 7:
+        digitalWrite(23, 0);
+        digitalWrite(24, 0);
+        digitalWrite(25, 0);
+        digitalWrite(26, 0);
+        digitalWrite(27, 0);
+        digitalWrite(28, 0);
+        digitalWrite(29, 1);
+        digitalWrite(10, 0);
+        break;
+      case 8:
+        digitalWrite(23, 0);
+        digitalWrite(24, 0);
+        digitalWrite(25, 1);
+        digitalWrite(26, 0);
+        digitalWrite(27, 0);
+        digitalWrite(28, 0);
+        digitalWrite(29, 0);
+        digitalWrite(10, 1);
+        break;
+    }
+    switch (punteo2) {
+      case 1:
+        digitalWrite(2, 1);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        break;
+      case 2:
+        digitalWrite(2, 0);
+        digitalWrite(3, 1);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        break;
+      case 3:
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 1);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        break;
+      case 4:
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 1);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        break;
+      case 5:
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 1);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        break;
+      case 6:
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 1);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        break;
+      case 7:
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 1);
+        digitalWrite(9, 0);
+        break;
+      case 8:
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 1);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 1);
+        break;
+    }
 
-    if ((punteo1 > 8) || (punteo2 > 8) ) {
+
+    if ((punteo1 >= 8) || (punteo2 >= 8) ) {
       digitalWrite(rojo, 1);
       digitalWrite(verde, 0);
       digitalWrite(azul, 1);
@@ -85,7 +291,9 @@ void loop() {
         digitalWrite(rojo, 1);
         digitalWrite(verde, 0);
         digitalWrite(azul, 0);
+        Serial.println(" Juagador 1 es el GANADOR \n");
       } else {
+        Serial.println(" Juagador 2 es el GANADOR \n");
         digitalWrite(rojo, 0);
         digitalWrite(verde, 1);
         digitalWrite(azul, 0);
@@ -97,6 +305,9 @@ void loop() {
       digitalWrite(rojo, 0);
       digitalWrite(verde, 0);
       digitalWrite(azul, 0);
+
+      Serial.println("Presiona cualquier boton para iniciar el juego");
+      Serial.println("ganara el primero en presionar 8 veces \n");
     }
   }
 }
